936abb130b6c54462cb158dbf100baaf6aa68507
jimmy.chou <nkai940372@gmail.com>
2025-03-08 18:15:32 +0800

    fix: add zbpack.json

--- start.sh
+++ start.sh
@@ -4,9 +4,11 @@
 cd api
 npm install
 npm run start &
+echo "Backend started on port 5001"
 
 # Navigate to frontend, install dependencies, build, and start it
 cd ../ui
 npm install
 npm run build
 npm run start
+echo "Frontend started on port 8080"
